6713cd4a135c65795b51da18986c5206971d63ec
Muhammet Bozkaya <muhammetbozkaya20@gmail.com>
2021-03-14 14:04:53 +0300

    theory data

--- wordmeister-api-test/Tests/Services/UserServiceTest.cs
+++ wordmeister-api-test/Tests/Services/UserServiceTest.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using wordmeister_api.Services;
 using wordmeister_api_test.Fixture;
+using wordmeister_api_test.Theory;
 using Xunit;
 
 namespace wordmeister_api_test.Tests.Services
@@ -10,37 +12,42 @@ namespace wordmeister_api_test.Tests.Services
     public class UserServiceTest : IClassFixture<ServiceFixture>
     {
         private UserService userService;
+        private IConfiguration config;
         public UserServiceTest(ServiceFixture _serviceFixture)
         {
             userService = _serviceFixture.userService;
+            config = _serviceFixture.config;
         }
 
         //public void Method_Scenario_ExpectedResult()
 
-        [Fact]
-        public void Authenticate_WithCorrectData_Success()
+        [Theory]
+        [ClassData(typeof(UserServiceTheory.AuthenticateRequestSuccess))]
+        public void Authenticate_WithCorrectData_Success(wordmeister_api.Dtos.Account.AuthenticateRequest model)
         {
-            wordmeister_api.Dtos.Account.AuthenticateResponse response = userService.Authenticate(new wordmeister_api.Dtos.Account.AuthenticateRequest
-            {
-                Email = "sadfaaf@tessfsdfsdt.com",
-                Password = "!&sdgdfgtest123",
-            });
+            wordmeister_api.Dtos.Account.AuthenticateResponse response = userService.Authenticate(model);
 
-            string firstName = "FirstName", LastName = "LastName";
+            string firstName = config["Mock:User:FirstName"], LastName = config["Mock:User:LastName"];
 
             Assert.NotNull(response);
             Assert.Equal(response.FirstName, firstName);
             Assert.Equal(response.LastName, LastName);
         }
 
-        [Fact]
-        public void Authenticate_WithIncorrectData_Null()
+        [Theory]
+        [ClassData(typeof(UserServiceTheory.AuthenticateRequestFailWithWrongEmail))]
+        public void Authenticate_WithWrongEmail_Null(wordmeister_api.Dtos.Account.AuthenticateRequest model)
         {
-            wordmeister_api.Dtos.Account.AuthenticateResponse response = userService.Authenticate(new wordmeister_api.Dtos.Account.AuthenticateRequest
-            {
-                Email = "sadfaaf@tessfsdfsdt.com",
-                Password = "1111",
-            });
+            wordmeister_api.Dtos.Account.AuthenticateResponse response = userService.Authenticate(model);
+
+            Assert.Null(response);
+        }
+
+        [Theory]
+        [ClassData(typeof(UserServiceTheory.AuthenticateRequestFailWithWrongPassword))]
+        public void Authenticate_WithWrongPassword_Null(wordmeister_api.Dtos.Account.AuthenticateRequest model)
+        {
+            wordmeister_api.Dtos.Account.AuthenticateResponse response = userService.Authenticate(model);
 
             Assert.Null(response);
         }
---
+++ wordmeister-api-test/Theory/UserServiceTheory.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using wordmeister_api.Dtos.Account;
+using wordmeister_api_test.Fixture;
+using Xunit;
+
+namespace wordmeister_api_test.Theory
+{
+    public class UserServiceTheory
+    {
+        public class AuthenticateRequestSuccess : TheoryData<AuthenticateRequest>
+        {
+            public AuthenticateRequestSuccess()
+            {
+                var config = HelperMethods.GetConfiguration();
+                Add(new AuthenticateRequest
+                {
+                    Email = config["Mock:User:Email"],
+                    Password = config["Mock:User:Password"],
+                });
+            }
+        }
+
+        public class AuthenticateRequestFailWithWrongEmail : TheoryData<AuthenticateRequest>
+        {
+            public AuthenticateRequestFailWithWrongEmail()
+            {
+                var config = HelperMethods.GetConfiguration();
+                Add(new AuthenticateRequest
+                {
+                    Email = "email",
+                    Password = config["Mock:User:Password"],
+                });
+            }
+        }
+
+        public class AuthenticateRequestFailWithWrongPassword : TheoryData<AuthenticateRequest>
+        {
+            public AuthenticateRequestFailWithWrongPassword()
+            {
+                var config = HelperMethods.GetConfiguration();
+                Add(new AuthenticateRequest
+                {
+                    Email = config["Mock:User:Email"],
+                    Password = "password",
+                });
+            }
+        }
+    }
+}
