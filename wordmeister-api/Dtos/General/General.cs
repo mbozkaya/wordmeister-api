45cabb7d7b1a6b360c3a745f3802a3b22b531c8b
Muhammet Bozkaya <muhammetbozkaya20@gmail.com>
2021-03-24 23:09:25 +0300

    cache

--- wordmeister-api/Dtos/General/General.cs
+++ wordmeister-api/Dtos/General/General.cs
@@ -13,37 +13,12 @@ namespace wordmeister_api.Dtos.General
             public object Data { get; set; }
             public string ErrorMessage { get; set; }
         }
+
+        public class CacheObject
+        {
+            public int Count { get; set; }
+            public DateTime CacheDate { get; set; }
+        }
     }
 
-    public enum HttpResponseCode
-    {
-        /// <summary>
-        /// Response to a successful GET, PUT, PATCH or DELETE. Can also be used for a POST that doesn't result in a creation.
-        /// </summary>
-        OK = 200,
-        /// <summary>
-        /// This means that client-side input fails validation.
-        /// </summary>
-        BADREQUEST = 400,
-        /// <summary>
-        /// This means the user isn’t not authorized to access a resource. It usually returns when the user isn’t authenticated.
-        /// </summary>
-        UNAUTHORIZED = 401,
-        /// <summary>
-        /// This means the user is authenticated, but it’s not allowed to access a resource.
-        /// </summary>
-        FORBIDDEN = 403,
-        /// <summary>
-        ///  This indicates that a resource is not found
-        /// </summary>
-        NOTFOUND = 404,
-        /// <summary>
-        /// This is a generic server error. It probably shouldn’t be thrown explicitly.
-        /// </summary>
-        INTERNALSERVERERROR = 500,
-        /// <summary>
-        ///  This indicates an invalid response from an upstream server.
-        /// </summary>
-        BADGATEWAY = 502,
-    }
 }
