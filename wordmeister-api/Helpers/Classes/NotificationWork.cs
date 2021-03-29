5bb6e07d52556d2b7ad511cb6cebee1433491bdb
Muhammet Bozkaya <muhammetbozkaya20@gmail.com>
2021-03-29 23:34:03 +0300

    job update

---
+++ wordmeister-api/Helpers/Classes/NotificationWork.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using wordmeister_api.Dtos.General;
+using wordmeister_api.Entity;
+using wordmeister_api.Model;
+
+namespace wordmeister_api.Helpers.Classes
+{
+    public class NotificationWork
+    {
+        WordmeisterContext _wordMeisterDbContext;
+        DateTime _time;
+        private readonly ILogger<NotificationJob> _logger;
+        private Mail _mailHelper;
+
+        public NotificationWork(WordmeisterContext wordMeisterDbContext, DateTime time, ILogger<NotificationJob> logger, Mail mailHelper)
+        {
+            _wordMeisterDbContext = wordMeisterDbContext;
+            _time = time;
+            _logger = logger;
+            _mailHelper = mailHelper;
+
+            try
+            {
+                Start();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error occured during notification job working at {_time:hh:mm:ss}.");
+            }
+        }
+
+
+        public void Start()
+        {
+
+            var currentNotifyUsers = GetNotificationUsers();
+
+            foreach (var user in currentNotifyUsers)
+            {
+                var userWordSentences = GetUserNotNotifiedSentences(user.Id, _time);
+
+                var template = CreateTemplateForUser(userWordSentences, user);
+
+                _mailHelper.Message(template, "", user.Email);
+
+                var mailLogId = MailLog(user.Id);
+
+                UserWordLog(userWordSentences, mailLogId);
+            }
+        }
+
+        private List<User> GetNotificationUsers()
+        {
+            return _wordMeisterDbContext.Users
+                .Where(w =>
+                w.UserSettings
+                .Where(w2 => w2.UserSettingTypeId == (int)Enums.SettingType.MailNotification
+                    && w2.Enable)
+                    .Any()
+                && w.UserInformations
+                    .Where(w3 => w3.NotificationHour == _time.Hour
+                    && w3.NotificationMinute == _time.Minute)
+                    .Any()
+            )
+                .ToList();
+        }
+
+        private List<JobDto.UserSentencesByWord> GetUserNotNotifiedSentences(long userId, DateTime notifyDate)
+        {
+            return _wordMeisterDbContext.UserWords
+                .Where(w => w.UserId == userId && w.CreatedDate.Date == notifyDate.Date)
+                .Select(s => new JobDto.UserSentencesByWord
+                {
+                    UserWordId = s.Id,
+                    Word = s.Word,
+                    Sentences = s.Word.Sentences.Where(w => w.UserId == null || w.UserId == userId).ToList()
+                })
+                .AsNoTracking()
+                .ToList();
+        }
+
+        private string CreateTemplateForUser(List<JobDto.UserSentencesByWord> wordSentences, User user)
+        {
+            string template = string.Empty;
+            foreach (var wordSentence in wordSentences)
+            {
+                template += wordSentence.Word.Text;
+            }
+
+            return template;
+        }
+
+        private long MailLog(long userId)
+        {
+            var mailLog = new MailLog
+            {
+                CreatedDate = _time,
+                UserId = userId,
+            };
+            _wordMeisterDbContext.MailLogs.Add(mailLog);
+
+            return mailLog.Id;
+        }
+
+        private void UserWordLog(List<JobDto.UserSentencesByWord> wordSentences, long mailLogId)
+        {
+            foreach (var sentence in wordSentences)
+            {
+                _wordMeisterDbContext.MailUserWords.Add(new Model.MailUserWord
+                {
+                    MailLogId = mailLogId,
+                    UserWordId = sentence.UserWordId,
+                });
+            }
+            _wordMeisterDbContext.SaveChanges();
+        }
+    }
+}
--- wordmeister-api/Helpers/NotificationJob.cs
+++ wordmeister-api/Helpers/NotificationJob.cs
@@ -8,6 +8,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using wordmeister_api.Dtos.General;
 using wordmeister_api.Entity;
+using wordmeister_api.Helpers.Classes;
 using wordmeister_api.Interfaces;
 using wordmeister_api.Model;
 using wordmeister_api.Services;
@@ -17,19 +18,21 @@ namespace wordmeister_api.Helpers
     public class NotificationJob : CronJobService
     {
         private WordmeisterContext _wordMeisterDbContext;
-        private DateTime _jobTime;
 
         private readonly ILogger<NotificationJob> _logger;
         private IScheduleConfig<NotificationJob> _config;
+        private Mail mailHelper; 
 
         public NotificationJob(IScheduleConfig<NotificationJob> config, ILogger<NotificationJob> logger, IServiceScopeFactory serviceScopeFactory)
             : base(config.CronExpression, config.TimeZoneInfo)
         {
             _logger = logger;
             _config = config;
+
             using (var scope = serviceScopeFactory.CreateScope())
             {
                 _wordMeisterDbContext = scope.ServiceProvider.GetService<WordmeisterContext>();
+                mailHelper = scope.ServiceProvider.GetService<Helpers.Classes.Mail>();
             }
         }
 
@@ -41,9 +44,7 @@ namespace wordmeister_api.Helpers
 
         public override Task DoWork(CancellationToken cancellationToken)
         {
-            Console.WriteLine($"{DateTime.Now:hh:mm:ss} CronJob 1 is working.");
-
-            _jobTime = DateTime.Now;
+            Task.Run(() => { new NotificationWork(_wordMeisterDbContext, DateTime.Now, _logger, mailHelper); });
 
             return Task.CompletedTask;
         }
@@ -52,68 +53,6 @@ namespace wordmeister_api.Helpers
         {
             _logger.LogInformation("CronJob 1 is stopping.");
             return base.StopAsync(cancellationToken);
-        }
-
-        public void Start()
-        {
-
-            var currentNotifyUsers = GetNotificationUsers();
-
-            foreach (var user in currentNotifyUsers)
-            {
-                var userWordSentences = GetUserNotNotifiedSentences(user.Id, _jobTime);
-
-                var template = CreateTemplateForUser(userWordSentences, user);
-                MailLog(userWordSentences);
-
-                //TODO send mail to user
-            }
-        }
-
-        private List<User> GetNotificationUsers()
-        {
-            return _wordMeisterDbContext.Users
-                .Where(w =>
-                w.UserSettings
-                .Where(w2 => w2.UserSettingTypeId == (int)Enums.SettingType.MailNotification
-                    && w2.Enable)
-                    .Any()
-                && w.UserInformations
-                    .Where(w3 => w3.NotificationHour == _jobTime.Hour
-                    && w3.NotificationMinute == _jobTime.Minute)
-                    .Any()
-            )
-                .ToList();
-        }
-
-        private List<JobDto.UserSentencesByWord> GetUserNotNotifiedSentences(long userId, DateTime notifyDate)
-        {
-            return _wordMeisterDbContext.UserWords
-                .Where(w => w.UserId == userId && w.CreatedDate.Date == notifyDate.Date)
-                .GroupBy(g => g.Word)
-                .Select(s => new JobDto.UserSentencesByWord
-                {
-                    Word = s.Key,
-                    Sentences = s.Key.Sentences.Where(w => w.UserId == null || w.UserId == userId).ToList()
-                })
-                .AsNoTracking()
-                .ToList();
-        }
-
-        private string CreateTemplateForUser(List<JobDto.UserSentencesByWord> wordSentences, User user)
-        {
-            string template = string.Empty;
-            foreach (var wordSentence in wordSentences)
-            {
-                template += wordSentence.Word.Text;
-            }
-
-            return template;
-        }
-
-        private async void MailLog(List<JobDto.UserSentencesByWord> wordSentences)
-        {
-
         }
     }
 }
